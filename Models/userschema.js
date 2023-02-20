d6a86f1e8fbf59a64753cfe328fe5e5b742bef2c
neeraj dhore <110654551+dhoreneeraj@users.noreply.github.com>
2023-02-20 07:15:17 +0000

    methods in userschma for token and compare password

--- Models/userschema.js
+++ Models/userschema.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import Authroles from '../utils/authRoles'
+import bycrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import crypto from "crypto"
+import config from "../config/index"
 
 const userSchema = mongoose.Schema ( 
     {
@@ -32,5 +36,41 @@ const userSchema = mongoose.Schema (
     timestamps:true,
 }
 );
+
+//challenge no -1 encrypt the password before saving mongosse middle ware pre hook
+
+userSchema.pre("save",async function(next){
+    if(!this.modified("password")) return next();
+    this.Password = await bycrypt.hash(this.Password, 10)
+    next()
+})
+
+// add more features to your user schema
+
+userSchema.methods = {
+//compare password
+ comparePassword: async function(enteredPassword){
+ return await bycrypt.compare(enteredPassword,this.Password)
+},
+ 
+//generate jwt token 
+
+getJwtToken: function(){
+   return JWT.sign(
+    {
+        _id:this._id,
+        role:this.role
+    },
+    config.JWT_SECRET,
+    {
+       expiresIn: config.JWT_EXPIRY
+    }
+   )
+
+}
+
+}
+
+
 
 export default mongoose.model("User",userSchema)
