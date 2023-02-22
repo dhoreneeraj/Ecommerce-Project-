b2581f7720f7d0452b2704d87c50d799d1f33194
neeraj dhore <110654551+dhoreneeraj@users.noreply.github.com>
2023-02-22 05:33:46 +0000

    coupon schema created

---
+++ Models/collection.schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const collectionSchema = new mongoose.Schema(
+ {
+    name:{
+        type:String,
+        required: [true,"Please provide the category name"],
+        trim:true,
+        maxLength:[120,"collection name should not be more than 120 characters"]
+    },
+ },
+ {
+    timestamps:true
+ }
+
+);
+
+export default mongoose.model("Collection",collectionSchema)
